106d62dbe8d7d3e50c16db0c8d3e815dee4c5e40
Henry Palonen <henry.palonen@palonenlabs.com>
2020-03-27 18:54:32 +0200

    + For github

---
+++ open-ventilator-tester/DataField.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace open_ventilator_tester
+{
+    public class DataField
+    {
+        public string Name { get; set; }
+        public int Index { get; set; }
+
+        public DataField(string name, int index)
+        {
+
+            this.Name = name;
+            this.Index = index;
+
+        }
+    }
+}
---
+++ open-ventilator-tester/DataPoint.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace open_ventilator_tester
+{
+    class DataPoint
+    {
+        public double y { get; set; }
+        public long x { get; set; }
+        public DataPoint(double value)
+        {
+            this.y = value;
+            this.x = DateTime.Now.Ticks;
+        }
+    }
+}
