8254db915c09d51a667cb518a07ab2d3972d1997
Henry Palonen <henry.palonen@palonenlabs.com>
2020-03-27 19:12:43 +0200

    + Arduino

---
+++ openventilator-testbench-arduino/openventilator-testbench-arduino/openventilator-testbench-arduino.ino
@@ -0,0 +1,32 @@
+//
+// Very simple software to control R/C ESC from serial port
+//
+// Henry Palonen / 25.3.2020
+
+#include <Servo.h>
+
+Servo ESC;     // create servo object to control the ESC
+
+int outValue;  // value from the analog pin
+
+void setup() {
+  // Attach the ESC on pin 9
+  ESC.attach(9,1000,2000); // (pin, min pulse width, max pulse width in microseconds) 
+  Serial.begin(9600);
+}
+
+void loop() {
+
+  // check if data has been sent from the computer:
+  if (Serial.available()) {
+    
+    // read the most recent byte (which will be from 0 to 255):
+    outValue = Serial.parseInt();
+    Serial.println(outValue);
+
+     if (outValue > -1 && outValue < 181)
+     {
+        ESC.write(outValue);    // Send the signal to the ESC
+     }
+  }
+}
